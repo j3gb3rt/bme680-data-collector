756ae16df39abee7cd48b8f61e7039066f5059b2
Jonathan Egbert <jegbert.eggy@gmail.com>
2019-05-17 02:19:07 -0400

    Write a full file and one for each minute

--- datacollector.py
+++ datacollector.py
@@ -9,17 +9,38 @@ bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c, debug=False)
 
 bme680.sea_level_pressure = 1013.25
 
+def writeFirstLine(ref):
+    ref.write("time, temp (C), gas (ohm), humidity (%), pressure (hPa), altitude (meters)\r\n")
+
 #open file here
-filename = str(datetime.now()) + ".csv"
-fileRef = open(filename, "w+")
+filename = "full" + str(datetime.now()) + ".csv"
+fullFileRef = open(filename, "w+")
+writeFirstLine(fullFileRef)
+
+minuteTimer = 60
 
 while True:
+    if (minuteTimer == 60):
+        filename = str(datetime.now()) + ".csv"
+        newRef = open(filename, "w+")
+        writeFirstLine(newRef)
+        newRef.close()
+        minuteTimer = 0
+
     print("\nTemperature: %0.1f C" % bme680.temperature)
     print("Gas: %d ohm" % bme680.gas)
     print("Humidity: %0.1f %%" % bme680.humidity)
     print("Pressure: %0.3f hPa" % bme680.pressure)
     print("Altitude: %0.2f meters" % bme680.altitude)
 
-    fileRef.write("{}, {}, {}, {}, {}\r\n".format(bme680.temperature, bme680.gas, bme680.humidity, bme680.pressure, bme680.altitude))
+    data = "{}, {}, {}, {}, {}, {}\r\n".format(datetime.now(), bme680.temperature, bme680.gas, bme680.humidity, bme680.pressure, bme680.altitude)
+    
+    fullFileRef.write(data)
 
+    fileRef = open(filename, "a+")
+    fileRef.write(data)
+    fileRef.close()
+
+    minuteTimer += 1
     time.sleep(1)
+
